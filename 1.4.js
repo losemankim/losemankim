e55814edf35b424f732be20ad73403888676f368
losemankim <80498324+losemankim@users.noreply.github.com>
2022-11-22 10:09:50 +0900

    Update 1.4.js

--- 1.4.js
+++ 1.4.js
@@ -1,8 +1,8 @@
-let RpcClient = require(“bitcoind-rpc-client”);
+let RpcClient = require("bitcoind-rpc-client");
 let client = new RpcClient( {
-     user: “test”,
-     pass: “test”,
-     host: “127.0.0.1”,
+     user: "test",
+     pass: "test",
+     host: "127.0.0.1",
      port: 12345
 });
 
