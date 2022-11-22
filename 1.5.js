06b88c19ad11be3a4293b4bb0bec2e16efcbc18d
losemankim <80498324+losemankim@users.noreply.github.com>
2022-11-22 09:56:31 +0900

    Create 1.5.js

---
+++ 1.5.js
@@ -0,0 +1,14 @@
+let RpcClient = require(“bitcoind-rpc-client”);
+let client = new RpcClient( {
+     user: “test”,
+     pass: “test”,
+     host: “127.0.0.1”,
+     port: 12345
+});
+(async function() {
+   let blockNumber = await client.generate(102);
+   console.log(blockHash);
+   
+   let blockNumber = await client.getBlock(blockHash.result[0]);
+   console.log(blockNumber);
+}) ();
