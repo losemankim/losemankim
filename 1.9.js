244c14078aadfc2e5c3ad53966f59b58139ded9e
losemankim <80498324+losemankim@users.noreply.github.com>
2022-11-22 10:12:40 +0900

    Update 1.9.js

--- 1.9.js
+++ 1.9.js
@@ -1,13 +1,13 @@
-let RpcClient = require(“bitcoind-rpc-client”);
+let RpcClient = require("bitcoind-rpc-client");
 let client = new RpcClient( {
-     user: “test”,
-     pass: “test”,
-     host: “127.0.0.1”,
+     user: "test",
+     pass: "test",
+     host: "127.0.0.1",
      port: 12345
 });
 (async function() {
-     let from = “”;
-     let to = “sonny”;
+     let from = "";
+     let to = "sonny";
      let amount = 5;
      let result = await client.move(from, to, amount);
      let accounts = await client.listAccounts();
